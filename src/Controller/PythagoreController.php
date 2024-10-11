2c56ca123984228eb868f053adebc0af1b689775
[Valentin] <[valentin.blin@etudiant.univ-rennes.fr]>
2024-10-11 17:07:07 +0200

    test

--- src/Controller/PythagoreController.php
+++ src/Controller/PythagoreController.php
@@ -1,21 +1,26 @@
 <?php
 
-<?php
-
 namespace App\Controller;
 
 use App\Action\DisplayPythagoreAction;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class PythagoreController extends AbstractController {
+    private $displayPythagoreAction;
+
+    // Injecter l'action via le constructeur
+    public function __construct(DisplayPythagoreAction $displayPythagoreAction) {
+        $this->displayPythagoreAction = $displayPythagoreAction;
+    }
+
     /**
      * @Route("/pythagore/view", name="pythagore_view")
      */
-    public function view() {
-        $action = new DisplayPythagoreAction();
-        return $action->execute($this->get('twig'));
+    public function view(): Response {
+        // Exécuter l'action et retourner le résultat dans la vue
+        return $this->displayPythagoreAction->execute();
     }
 }
 
-?>
